e746d4ed202b37c655ca7d12b8718b02dfd18563
patdz <patdz@ymipro.com>
2020-12-16 17:26:34 +0800

    init
    Signed-off-by: patdz <patdz@ymipro.com>

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # homebrew-influx-cli
+
+
+
+## install
+
+```
+brew tap patdz/influx-cli
+brew update && brew doctor
+brew install influx-cli
+```
+
---
+++ influx-cli.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class InfluxCli < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/patdz/influx-cli/releases/download/v0.0.1/0.0.1.tgz"
+  sha256 "3c613c79c2919398aef2df397381ae1075a60f1e70b5bcac4a127297fdb7572f"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "cmake", ".", *std_cmake_args
+    bin.install "influx"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test influx-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
